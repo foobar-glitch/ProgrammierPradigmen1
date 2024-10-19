bc78f930f1c9e5f1bf4e4ac497241dcae44ef31f
julianschloer <e12322028@student.tuwien.ac.at>
2024-10-19 21:26:49 +0200

    Added times and copy function in MaterialBag

--- src/MaterialBag.java
+++ src/MaterialBag.java
@@ -47,6 +47,39 @@ public class MaterialBag {
         }
     }
 
+    /**
+     * This function makes a copy of the object in which the amount of every
+     * Material is changed by the factor in param multiplier
+     *
+     * @param multiplier Factor with which the amount of every material is multiplied
+     * @return New, copied and (amount by multiplier) changed MaterialBag-Object
+     */
+    public MaterialBag times(float multiplier){
+        MaterialBag tmp = new MaterialBag();
+        for(Material m : materialInventory.keySet()){
+            tmp.setMaterial(
+                    m,
+                    materialInventory.get(m)*multiplier
+            );
+        }
+        return tmp;
+    }
+
+    /**
+     * Copies an MaterialBag-Object
+     * @return Exact Copy of original Object
+     */
+    public MaterialBag copy(){
+        MaterialBag tmp = new MaterialBag();
+        for(Material m : materialInventory.keySet()){
+            tmp.setMaterial(
+                    m,
+                    materialInventory.get(m)
+            );
+        }
+        return tmp;
+    }
+
     public CostContainer getTotalCost() {
         return totalCost;
     }
