dc2dbccd663d3e3cae7b07b6947261a7de219d8a
mar-abc123 <mygarbagemailbox1@gmail.com>
2024-10-20 21:59:59 +0200

    updated Simulation

--- src/Simulation.java
+++ src/Simulation.java
@@ -17,32 +17,28 @@ public class Simulation {
     public SimulationResult runSimulation() {
         while (building.checkAge()) {
             CostContainer costsThisYear = new CostContainer(0.0f, 0.0f, 0.0f);
-            costsThisYear.addCostContainer(building.betriebskosten());
-            happinessPerYear.add(building.zufriedenheit());
+            happinessPerYear.add(building.satisfaction());
 
             // amount of renovations will be correctly scaled for number of apartment and
             // the time it should take for a full renovation by the gebauede
             // random function can be anything as long as expected value = 1
             float renovations = (float) (Math.random() * 2.0);
-            costsThisYear.addCostContainer(building.renovieren(renovations));
+            costsThisYear.addCostContainer(building.renovate(renovations));
 
             double randomVal = Math.random();
             if (randomVal < 0.05) {
                 if (randomVal < 0.005) {
+                    costsThisYear.addCostContainer(building.demolishing());
+                    costsPerYear.add(costsThisYear);
                     break;
                 }
                 // TODO replace/make up formula
-                costsThisYear.addCostContainer(building.renovieren(10));
+                costsThisYear.addCostContainer(building.renovate(20.0f));
             }
 
+            costsThisYear.addCostContainer(building.age());
             costsPerYear.add(costsThisYear);
-
-            building.age();
         }
-
-        costsPerYear.get(0).addCostContainer(building.hausbauen());
-        costsPerYear.get(costsPerYear.size() - 1).addCostContainer(building.abriss());
-
         return new SimulationResult(costsPerYear, happinessPerYear);
     }
 
