000d754818564891f3005161ca71e30d1bd4f7b5
foobar-glitch <hamed.asami@rub.de>
2024-10-21 13:38:41 +0200

    Fixed tests

--- src/Test.java
+++ src/Test.java
@@ -3,29 +3,31 @@ hier ueber die Aufteilung der Arbeit etc. schreiben
 
 Julian - Apartments, ApartmentSpecs, MaterialBag
 
-Marian - Main, Simulation, SimulationResult
+Marian - Main, Simulation, SimulationResult, BuildingSpecs
 
 Hamed - CostContainer, Building, Material
 
  */
 
+import java.rmi.UnexpectedException;
+
 public class Test {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnexpectedException {
         Main mainFunction = new Main();
         Material[] materials = new Material[] {
                 new Material("wood", new CostContainer(125.0, 0.4, 0.01)),
                 new Material("concrete", new CostContainer(250.0, 0.8, 0.15)),
                 new Material("steel", new CostContainer(1200.0, 1.4, 0.007))
         };
-        int numberOfApartments = 10, lifetimeBuilding = 50;
+        int numberOfApartments = 10, lifetimeBuilding = 50, residentsPApartment = 1;
         MaterialBag shellEco = new MaterialBag(materials, new Double[] {257.3, 10.0, 2.45});
         MaterialBag  interiorEco = new MaterialBag(materials, new Double[] {14.4, 2.0, 0.875});
         CostContainer heatingAndMaintenanceCostsEco = new CostContainer(0.35, 0.3, 0.05);
-        ApartmentSpecs apartmentsEco = new ApartmentSpecs(interiorEco, 1, numberOfApartments, 20,0.9);
+        ApartmentSpecs apartmentsEco = new ApartmentSpecs(interiorEco, residentsPApartment, numberOfApartments, 20,0.9);
         BuildingSpecs buildingEco = new BuildingSpecs(lifetimeBuilding, shellEco, apartmentsEco, heatingAndMaintenanceCostsEco,0.7f);
 
-        Simulation simulation = new Simulation(buildingEco, buildingEco.getApartmentSpecs());
+        Simulation simulation = new Simulation(buildingEco, apartmentsEco);
         SimulationResult simResult = simulation.runSimulation();
 
 
@@ -36,12 +38,28 @@ public class Test {
         CostContainer costLowerBound = shellCosts.addCostContainer(
                 interiorCosts.multiplyContainer(numberOfApartments)
         );
+        // divide by number of residents
+        costLowerBound = costLowerBound.multiplyContainer((float) 1/(numberOfApartments*residentsPApartment*lifetimeBuilding));
 
         CostContainer costUpperBound = shellCosts.addCostContainer(
                 interiorCosts.multiplyContainer(numberOfApartments*lifetimeBuilding)
-        );
+        ).multiplyContainer((float) 1/numberOfApartments*residentsPApartment*lifetimeBuilding);
 
+        if(costLowerBound.getCost() > simResult.getAverageCostOverLifetime()
+                || costLowerBound.getCo2() > simResult.getAverageCo2OverLifetime()
+                || costLowerBound.getWaste() > simResult.getAverageWasteOverLifetime()
+        ) {
+            throw new UnexpectedException("The results are too small.");
+        }
 
+        if(costUpperBound.getCost() < simResult.getAverageCostOverLifetime()
+                || costUpperBound.getCo2() < simResult.getAverageCo2OverLifetime()
+                || costUpperBound.getWaste() < simResult.getAverageWasteOverLifetime()
+        ){
+            throw new UnexpectedException("The results are too big.");
+        }
+
+        System.out.println("Tests successful!");
 
     }
 }
